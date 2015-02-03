7ddd8e83b4ad0a614a1f418eddadf7a4f99c03d0
avallete <avallete@e1r4p3.42.fr>
2015-02-03 19:06:13 +0100

    jjjj

--- src/nova.c
+++ src/nova.c
@@ -6,7 +6,7 @@
 /*   By: avallete <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/03 13:58:09 by avallete          #+#    #+#             */
-/*   Updated: 2015/02/03 17:12:41 by avallete         ###   ########.fr       */
+/*   Updated: 2015/02/03 19:05:58 by avallete         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,16 @@ unsigned int	it_nova(t_nc z, t_nc c, t_mle *env)
 {
 	unsigned int cm;
 	long double	tmp;
+	long double f;
 
 	cm = 0;
 	tmp = 0;
-	while ((sqrt(z.r*z.r + z.i*z.i) < 2) && (cm < C_FR(env)->it))
+	f = z.r + z.i;
+	while (sqrt(c.r*c.r + c.i*c.i) < 4 && (cm < C_FR(env)->it))
 	{
 		tmp = z.r;
-		z.r = pow(z.r, 1.5) - pow(z.i, 1.5) + c.r;
-		z.i = pow(tmp, 1.5) * pow(z.i, 1.5) +  c.i;
+		z.r = (z.r * 0.33334) - 0.33334/z.r*z.r + c.r;
+		z.i = (1.5 * z.i * tmp) + c.i;
 		cm++;
 	}
 	return (cm);
@@ -45,8 +47,8 @@ void	create_nova(t_mle *env)
 		{
 			z.r = x / ZOOM_X(C_FR(env)->x1, C_FR(env)->x2) + C_FR(env)->x1;
 			z.i = y / ZOOM_Y(C_FR(env)->y1, C_FR(env)->y2) + C_FR(env)->y1;
-			c.r = 0.375 + C_IF(env)->cr;
-			c.i = 0.002 +  C_IF(env)->ci;
+			c.r = 0.06;
+			c.i =  0.0;
 			cm = it_nova(z, c, env);
 			init_colors(env, cm);
 			draw_to_img(env, PLACE_IMG(x, y), C_FR(env)->rgb);
